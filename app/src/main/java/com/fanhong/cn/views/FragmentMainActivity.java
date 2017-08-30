6ec080a78f4091e8c7b56b2fb74b16be8394a0a7
qingyuyefeng <97667024@qq.com>
2017-08-30 09:41:36 +0800

    import 01

---
+++ app/src/main/java/com/fanhong/cn/views/FragmentMainActivity.java
@@ -0,0 +1,10 @@
+package com.fanhong.cn.views;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by Administrator on 2017/8/30.
+ */
+
+public class FragmentMainActivity extends FragmentActivity{
+}
